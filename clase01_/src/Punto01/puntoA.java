ec85192e777c960b39aa4369d022f7741c9b0d15
TomasMassa <122112860+TomasM2002@users.noreply.github.com>
2023-03-08 12:13:20 -0300

    first commit
    clase 1

---
+++ clase01_/src/Punto01/puntoA.java
@@ -0,0 +1,19 @@
+package Punto01;
+
+public class puntoA {
+
+	public static void main(String[] args) {
+		int numeroInicio = 5;
+		int numeroFinal = 14;
+		
+		while (numeroInicio <= numeroFinal){
+			System.out.println( numeroInicio );
+			numeroInicio = numeroInicio + 1 ;
+			
+		}
+		
+		
+		
+	}
+
+}
---
+++ clase01_/src/Punto01/puntoB.java
@@ -0,0 +1,21 @@
+package Punto01;
+
+public class puntoB {
+
+	public static void main(String[] args) {
+		int numeroInicio = 5;
+		int numeroFinal = 14;
+		
+		while (numeroInicio <= numeroFinal){
+			if (numeroInicio % 2 == 0) {
+				System.out.println( numeroInicio );
+			}
+			numeroInicio = numeroInicio + 1 ;
+			
+		}
+		
+		
+		
+	}
+
+}
