e388583a40e210d4a1325a4b254797d84560b5b2
TomasMassa <122112860+TomasM2002@users.noreply.github.com>
2023-03-08 19:50:27 -0300

    tercer commit

---
+++ clase02_/src/clase02_/Punto01_.java
@@ -0,0 +1,17 @@
+package clase02_;
+
+public class Punto01_ {
+
+	public static void main(String[] args) {
+		String txt1 = "ahora" ;
+		int cantidad = 0 ;
+		int cantidadFinal = cantidad;
+		
+		//conteo de letras "a".
+	
+		System.out.println(txt1.charAt(0));
+		System.out.println(txt1.charAt(4));
+		System.out.println();
+	}
+
+}
