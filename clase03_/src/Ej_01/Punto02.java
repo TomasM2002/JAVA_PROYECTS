a4a000b856343d38357fe47b1dea36057d2bb7bf
TomasMassa <122112860+TomasM2002@users.noreply.github.com>
2023-03-09 23:02:27 -0300

    actualizacion

---
+++ clase03_/src/Ej_01/Punto02.java
@@ -0,0 +1,15 @@
+package Ej_01;
+import java.util.Arrays;
+
+public class Punto02 {
+	public static void main(String[] args) {
+		int numeros [] = new int [] {4,8,3};
+		Arrays.sort(numeros);
+		System.out.println(Arrays.toString(numeros));
+
+}
+
+	
+}
+
+	
---
+++ clase03_/src/Ej_01/Punto03.java
@@ -0,0 +1,32 @@
+package Ej_01;
+import java.util.Arrays;
+
+public class Punto03 {
+	public static void main(String[] args) {
+		int a [] = new int [] {55,1,8,13};
+		int x = 10;
+		int suma= 0;
+		
+		
+		
+        for (int i = 0; i < a.length ; i++) {
+        	if(a[i]>x) {
+        		suma += a[i];
+    		}
+        		
+        		 		
+        }
+        System.out.println("Array Sum = "+suma);
+        
+		
+		
+	
+			
+			
+
+			
+		
+		
+
+	}
+}
